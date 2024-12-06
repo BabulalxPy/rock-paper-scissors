aef0738bf0ca068e559442ad487c25fa0e734ba3
BabulalxPy <babulalchaudhari8329@gmail.com>
2024-12-06 18:51:36 +0530

    Added ui and made the game interactive

--- script.js
+++ script.js
@@ -4,90 +4,131 @@ function getComputerChoice(){
     return arr[(Math.floor(Math.random() * arr.length))];
 }
 
-function getHumanchoice(){
-    let ans = prompt("Enter your answer(rock,paper or scissors)")
-    return ans.toLowerCase();
-}
-
-
 let humanScore = 0;
 let computerScore = 0;
 
+
+const displayHumanScore = document.querySelector(".displayHumanScore");
+const displayComputerScore = document.querySelector(".displayComputerScore");
+
+function updateScore() {
+    displayHumanScore.textContent = "Human Score: " + humanScore;
+    displayComputerScore.textContent = "Computer Score: " + computerScore;
+};
+
+
 function playround(humanChoice, computerChoice){
+    const display = document.querySelector(".display");
+    const displayResults = document.querySelector(".displayResults");
     if(humanChoice == "rock" && computerChoice == "paper"){
-        console.log("You lose! Paper beats rock");
         computerScore += 1;
+        
+        displayResults.textContent = "You lose! Paper beats rock";
+        display.appendChild(displayResults);
+        
+        console.log("You lose! Paper beats rock");
+        
     }
     else if(humanChoice == "paper" && computerChoice == "scissors"){
-        console.log("You lose! Scissors beats paper");
         computerScore += 1;
+        
+        displayResults.textContent = "You lose! Scissors beats paper";
+        display.appendChild(displayResults);
+
+        console.log("You lose! Scissors beats paper");
     }
     else if(humanChoice == "scissors" && computerChoice == "rock"){
-        console.log("You lose! rock beats scissors");
         computerScore += 1;
+
+        displayResults.textContent = "You lose! rock beats scissors";
+        display.appendChild(displayResults);
+
+        console.log("You lose! rock beats scissors");
+        
     }
     else if(humanChoice == "rock" && computerChoice == "scissors"){
-        console.log("You won!");
         humanScore += 1;
+        
+        displayResults.textContent = "You won!";
+        display.appendChild(displayResults);
+
+        console.log("You won!");
+        
     }
     else if(humanChoice == "paper" && computerChoice == "rock"){
-        console.log("Your won!");
         humanScore += 1;
+        
+        displayResults.textContent = "Your won!";
+        display.appendChild(displayResults);
+
+        console.log("Your won!");
+        
     }
     else if(humanChoice == "scissors" && computerChoice == "paper"){
-        console.log("You won!");
         humanScore += 1;
+        
+        displayResults.textContent = "You won!";
+        display.appendChild(displayResults);
+
+        console.log("You won!");
+        
     }
     else if(humanChoice === computerChoice){
+        
+        displayResults.textContent = "Tie";
+        display.appendChild(displayResults);
+
         console.log("Tie");
     }
     else{
         console.log("Enter correctly");
     }
+
+    updateScore();
+
+    const declareWinner = document.querySelector(".declareWinner");
+
+    if(humanScore == 5){
+        declareWinner.textContent = "You Won the game!";
+        display.appendChild(declareWinner);
+        btn1.removeEventListener("click", rock);
+        btn2.removeEventListener("click", paper);
+        btn3.removeEventListener("click", scissors);
+    }
+    else if(computerScore == 5){
+        declareWinner.textContent = "You lost the game!";
+        display.appendChild(declareWinner);
+        btn1.removeEventListener("click", rock);
+        btn2.removeEventListener("click", paper);
+        btn3.removeEventListener("click", scissors);
+    }
+
 }
 
 
-
-function playgame(){
-    const humanSelection1 = getHumanchoice();
+function rock(){
+    humanChoice = "rock";
     const computerSelection1 = getComputerChoice();
-    console.log(playround(humanSelection1, computerSelection1));
-    console.log("Your score: ", humanScore);
-    console.log("Computer score: ", computerScore);
-    const humanSelection2 = getHumanchoice();
-    const computerSelection2 = getComputerChoice();
-    console.log(playround(humanSelection2, computerSelection2));
-    console.log("Your score: ", humanScore);
-    console.log("Computer score: ", computerScore);
-    const humanSelection3 = getHumanchoice();
-    const computerSelection3 = getComputerChoice();
-    console.log(playround(humanSelection3, computerSelection3));
-    console.log("Your score: ", humanScore);
-    console.log("Computer score: ", computerScore);
-    const humanSelection4 = getHumanchoice();
-    const computerSelection4 = getComputerChoice();
-    console.log(playround(humanSelection4, computerSelection4));
-    console.log("Your score: ", humanScore);
-    console.log("Computer score: ", computerScore);
-    const humanSelection5 = getHumanchoice();
-    const computerSelection5 = getComputerChoice();
-    console.log(playround(humanSelection5, computerSelection5));
-    console.log("Your score: ", humanScore);
-    console.log("Computer score: ", computerScore);
+    playround(humanChoice,computerSelection1);
+   
+}
+function paper(){
+    humanChoice = "paper";
+    const computerSelection1 = getComputerChoice();
+    playround(humanChoice,computerSelection1);
+
+}
+function scissors(){
+    humanChoice = "scissors";
+    const computerSelection1 = getComputerChoice();
+    playround(humanChoice,computerSelection1);
     
 }
-console.log(playgame())
-if (humanScore > computerScore){
-    console.log("You won the game");
-}
-else{
-    console.log("You lost the game");
-}
 
-const container = document.querySelector("#container");
+const btn1 = document.querySelector("#btn1");
+btn1.addEventListener("click", rock);
+const btn2 = document.querySelector("#btn2");
+btn2.addEventListener("click", paper);
+const btn3 = document.querySelector("#btn3");
+btn3.addEventListener("click", scissors);
 
-const content = document.createElement("div");
-content.classList.add("content");
-content.textContent = "This is the glorious text-content!";
-content.style.backgroundColor = 'blue';
-container.appendChild(content);
